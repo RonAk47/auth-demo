4a9a18dd63af5444c929cf99d325576638e5920d
Ronak Jain <rj.ronakjain98@gmail.com>
2026-06-06 12:27:39 +0530

    Implemented pagination and sorting on getAllImages route

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -46,13 +46,31 @@ const uploadImage = async (req, res) => {
 
 const fetchImages = async (req, res) => {
     try {
-        const images = await Image.find({});
+        //Pagination and sorting logic
+        const page = Number(req.query?.page) || 1;
+        const limit = Number(req.query?.limit) || 5;
+        const skip = (page - 1) * 5;
+        const sortBy = req.query?.sortBy || 'createdAt';
+        const sortOrder = req.query?.sortOrder === 'asc' ? 1 : -1;
+        const numberOfImages = await Image.countDocuments();
+        const totalPages = Math.ceil(numberOfImages / limit);
+        const sortObj = {
+            [sortBy]: sortOrder
+        };
+
+        const images = await Image.find({})
+            .sort(sortObj)
+            .skip(skip)
+            .limit(limit);
 
         return res.status(200).json({
             status: true,
             message: images.length
                 ? 'Images fetched successfully'
                 : 'No images found',
+            currentPage: page,
+            totalPages,
+            totalImages: numberOfImages,
             data: images
         });
     } catch (err) {
